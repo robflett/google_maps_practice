dc4063b3e60199b46187e3e51fe53e39ecc62b48
Rob <robflettcc@gmail.com>
2017-04-11 21:39:03 +0100

    buttons added

--- public/app.js
+++ public/app.js
@@ -3,11 +3,27 @@ var initialize = function(){
   var center = {lat: 58.552722, lng: -3.918369}
   var container = document.querySelector("#main-map");
   var mainMap = new MapWrapper(container, center, 12);
+  var forteviotButton = document.querySelector("#forteviot-button")
+  var melvichButton = document.querySelector("#melvich-button")
 
   mainMap.addMarker(center, mainMap);
   mainMap.addClickEvent();
   mainMap.addInfoWindow(center, "Melvich is a simple place")
 
+  var findForteviot = function(){
+    var forteviot = { lat: 56.340646, lng: -3.535601};
+    mainMap.googleMap.setCenter(forteviot);
+    mainMap.addInfoWindow(forteviot, "Forteviot is a special place");
+  }
+
+  var findMelvich = function(){
+    var melvich = {lat: 58.552722, lng: -3.918369};
+    mainMap.googleMap.setCenter(melvich);
+    mainMap.addInfoWindow(melvich, "Melvich is a simple place")
+  }
+
+forteviotButton.onclick = findForteviot;
+melvichButton.onclick = findMelvich;
 
 }
 window.onload = initialize;
